eeb842cf9fe88563ba5b45c78a791625db1b7921
Hasti Donga <hastidonga25@gmail.com>
2024-04-15 23:55:45 -0400

    Fetching data from API

--- JAVASCRIPT/js.js
+++ JAVASCRIPT/js.js
@@ -6,6 +6,11 @@ const submit = document.querySelector("#submit");
 // Async function to fetch data from the API
 async function getData(){
     try{
+        // Get the value of the input field and convert it to lowercase
+        const personName = document.getElementById("name").value.toLowerCase();
+        // Fetch data from the API using the entered name
+        const response = await fetch(`https://api.agify.io/?name=${personName}`);
+        
     }
     catch(error){
         console.log(error);
